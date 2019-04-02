9d0827e1cd3a8bc5d0a04466ca4192791ba3ef8c
David Piper <david.piper@mailfence.com>
2019-04-02 09:39:53 -0500

    test: reactor the tests to add more descriptive grouping
    Signed-off-by: David Piper <david.piper@mailfence.com>

--- src/__tests__/slack-notify-status.test.ts
+++ src/__tests__/slack-notify-status.test.ts
@@ -2,68 +2,72 @@
 
 import SlackNotifyStatus from '../slack-notify-status';
 
-describe('slack-notify-status tests', () => {
-  describe('< 1 second tests', () => {
-    test('getTimeTakenString should give correct ms for integer', () => {
-      expect(SlackNotifyStatus.getTimeTakenString(50)).toEqual(
-        '50 milliseconds',
-      );
+describe('SlackNotifyStatus static tests', () => {
+  describe('SlackNotifyStatus.getTimeTakenString tests', () => {
+    describe('< 1 second tests', () => {
+      test('getTimeTakenString should give correct ms for integer', () => {
+        expect(SlackNotifyStatus.getTimeTakenString(50)).toEqual(
+          '50 milliseconds',
+        );
+      });
+
+      test('exactly 1 ms', () => {
+        expect(SlackNotifyStatus.getTimeTakenString(1)).toEqual(
+          '1 millisecond',
+        );
+      });
+
+      test('getTimeTakenString should give correct ms for real number', () => {
+        expect(SlackNotifyStatus.getTimeTakenString(50.135)).toEqual(
+          '50.14 milliseconds',
+        );
+      });
     });
 
-    test('exactly 1 ms', () => {
-      expect(SlackNotifyStatus.getTimeTakenString(1)).toEqual('1 millisecond');
+    describe('>= 1 second && < 1 minute tests', () => {
+      test('getTimeTakenString should give correct seconds for integer', () => {
+        expect(SlackNotifyStatus.getTimeTakenString(50 * 1000)).toEqual(
+          '50 seconds',
+        );
+      });
+
+      test('getTimeTakenString should give correct seconds for real number', () => {
+        expect(SlackNotifyStatus.getTimeTakenString(50.135 * 1000)).toEqual(
+          '50.14 seconds',
+        );
+      });
+
+      test('exactly 1 second', () => {
+        expect(SlackNotifyStatus.getTimeTakenString(1 * 1000)).toEqual(
+          '1 second',
+        );
+      });
     });
 
-    test('getTimeTakenString should give correct ms for real number', () => {
-      expect(SlackNotifyStatus.getTimeTakenString(50.135)).toEqual(
-        '50.14 milliseconds',
-      );
-    });
-  });
+    describe('>= 1 minute tests', () => {
+      test('getTimeTakenString should give correct min and sec for integer', () => {
+        expect(SlackNotifyStatus.getTimeTakenString(50 * 1000 * 2)).toEqual(
+          '1 minute and 40 seconds',
+        );
+      });
 
-  describe('>= 1 second && < 1 minute tests', () => {
-    test('getTimeTakenString should give correct seconds for integer', () => {
-      expect(SlackNotifyStatus.getTimeTakenString(50 * 1000)).toEqual(
-        '50 seconds',
-      );
-    });
+      test('getTimeTakenString should give correct min and sec for real number', () => {
+        expect(SlackNotifyStatus.getTimeTakenString(50.135 * 1000 * 2)).toEqual(
+          '1 minute and 40.27 seconds',
+        );
+      });
 
-    test('getTimeTakenString should give correct seconds for real number', () => {
-      expect(SlackNotifyStatus.getTimeTakenString(50.135 * 1000)).toEqual(
-        '50.14 seconds',
-      );
-    });
+      test('exactly 1 minute', () => {
+        expect(SlackNotifyStatus.getTimeTakenString(1 * 1000 * 60)).toEqual(
+          '1 minute',
+        );
+      });
 
-    test('exactly 1 second', () => {
-      expect(SlackNotifyStatus.getTimeTakenString(1 * 1000)).toEqual(
-        '1 second',
-      );
-    });
-  });
-
-  describe('>= 1 minute tests', () => {
-    test('getTimeTakenString should give correct min and sec for integer', () => {
-      expect(SlackNotifyStatus.getTimeTakenString(50 * 1000 * 2)).toEqual(
-        '1 minute and 40 seconds',
-      );
-    });
-
-    test('getTimeTakenString should give correct min and sec for real number', () => {
-      expect(SlackNotifyStatus.getTimeTakenString(50.135 * 1000 * 2)).toEqual(
-        '1 minute and 40.27 seconds',
-      );
-    });
-
-    test('exactly 1 minute', () => {
-      expect(SlackNotifyStatus.getTimeTakenString(1 * 1000 * 60)).toEqual(
-        '1 minute',
-      );
-    });
-
-    test('plural minutes and integer ms', () => {
-      expect(SlackNotifyStatus.getTimeTakenString(50 * 1000 * 5)).toEqual(
-        '4 minutes and 10 seconds',
-      );
+      test('plural minutes and integer ms', () => {
+        expect(SlackNotifyStatus.getTimeTakenString(50 * 1000 * 5)).toEqual(
+          '4 minutes and 10 seconds',
+        );
+      });
     });
   });
 });
